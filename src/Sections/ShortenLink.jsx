a3c6b82dd6e27e8143dc00235edf0773ec534970
aridguy <50306833+aridguy@users.noreply.github.com>
2023-11-04 18:10:45 +0100

    modified the link shortner section

--- src/Sections/ShortenLink.jsx
+++ src/Sections/ShortenLink.jsx
@@ -10,8 +10,9 @@ const ShortenLink = () => {
             <div className="col-md-1"></div>
             <div className="col-md-10">
               <div className="shorten-bg">
-                <div>
-                
+                <div className="d-flex gap-3">
+                    <input width='' type="url" placeholder="shorten a link here " className="w-80 form-control text-danger linkShortner" />
+                    <button className="btn btn-secondary">Shorten it!</button>
                 </div>
               </div>
             </div>
